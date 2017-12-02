0091c1fd83ba040d4f33d229c6a99af1e34a812a
Benjamin M6 <benjamynfrank101@gmail.com>
2017-12-02 04:13:56 -0600

    Add constants for pet content URIs

--- app/src/main/java/com/example/android/pets/data/PetContract.java
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -1,5 +1,6 @@
 package com.example.android.pets.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -9,9 +10,35 @@ import android.provider.BaseColumns;
 
 public final class PetContract {
 
-    PetContract () {}
+    PetContract() {
+    }
 
-    public static final class PetEntry implements BaseColumns{
+    /**
+     * The "Content authority" is a name for the entire content provider, similar to the
+     * relationship between a domain name and its website.  A convenient string to use for the
+     * content authority is the package name for the app, which is guaranteed to be unique on the
+     * device.
+     */
+    public static final String CONTENT_AUTHORITY = "com.example.android.pets";
+
+    /**
+     * Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
+     * the content provider.
+     */
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    /**
+     * Possible path (appended to base content URI for possible URI's)
+     * For instance, content://com.example.android.pets/pets/ is a valid path for
+     * looking at pet data. content://com.example.android.pets/staff/ will fail,
+     * as the ContentProvider hasn't been given any information on what to do with "staff".
+     */
+    public static final String PATH_PETS = "pets";
+
+    public static final class PetEntry implements BaseColumns {
+
+        /** The content URI to access the pet data in the provider */
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI,PATH_PETS);
 
         public static final String TABLE_NAME = "pets"; // name of the table
 
@@ -21,7 +48,9 @@ public final class PetContract {
         public static final String COLUMN_PET_GENDER = "gender"; // Gender of the pet. Type: INTEGER
         public static final String COLUMN_PET_WEIGHT = "weight"; // Weight of the pet. Type: INTEGER
 
-        /** Possible values for gender of pets */
+        /**
+         * Possible values for gender of pets
+         */
         public static final int GENDER_MALE = 1;
         public static final int GENDER_FEMALE = 2;
         public static final int GENDER_UNKNOWN = 0;
